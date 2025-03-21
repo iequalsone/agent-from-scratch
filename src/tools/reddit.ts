909f44cd0dc105cf5e91cedacb3d15ec0737e660
Jon Howard <johnny.rtn@gmail.com>
2025-03-21 03:44:43 -0230

    Add new tools: generateImage, reddit, and dadJoke with integration into the agent

---
+++ src/tools/reddit.ts
@@ -0,0 +1,24 @@
+import type { ToolFn } from "../../types";
+import { z } from "zod";
+import fetch from 'node-fetch';
+
+export const redditToolDefinition = {
+  name: 'reddit',
+  parameters: z.object({}),
+  description: 'get latest post from reddit'
+}
+
+type Args = z.infer<typeof redditToolDefinition.parameters>
+export const reddit: ToolFn<Args, string> = async () => {
+  const { data } = await fetch('https://www.reddit.com/.json').then((res) => res.json())
+
+  const relevantInfo = data.children.map((child: any) => ({
+    title: child.data.title,
+    link: child.data.url,
+    subreddit: child.data.subreddit_name_prefixed,
+    author: child.data.author,
+    upvotes: child.data.ups
+  }));
+
+  return JSON.stringify(relevantInfo, null, 2);
+}
